02cde8f071ec74022f05a1371f02fdd69a5c320b
Andrew Gayle <agayle@gmail.com>
2022-12-03 14:45:23 -0500

    modified beforeEach() in unhappy.paths.cy.js and added new scripts to package.json

--- cypress/e2e/test/unhappy.paths.cy.js
+++ cypress/e2e/test/unhappy.paths.cy.js
@@ -14,21 +14,15 @@ describe('Sauce Labs Demo - Unhappy paths - Check out info', () => {
     beforeEach(() => {
         SauceLabsLogin.open();
         SauceLabsLogin.signIn(userCredentials[0].userName,userCredentials[0].password);
+
+         //add an item from product list to cart
+         SauceLabsProductPage.addToCart(productData.productList[0].name);
+         SauceLabsProductPage.navigateToCart(); 
+         cy.get(SauceLabsCart.btnCheckout).click();
     });       
 
     it(`should require a last name to be entered when checking out an item`, () => {
       
-        //add an item from product list to cart
-        SauceLabsProductPage.addToCart(productData.productList[0].name);
-        SauceLabsProductPage.navigateToCart();
-
-        cy.get(SauceLabsProductPage.spanShoppingCartBadge)
-        .should('exist');
-
-        cy.get(SauceLabsProductPage.spanShoppingCartBadge) // check that an item is in the cart
-        .should('have.text',1);
-
-        cy.get(SauceLabsCart.btnCheckout).click();
         SauceLabsCheckoutInfo.completeCheckoutFormWithoutLastName(faker.name.firstName(),faker.address.zipCode()); //enter customer info in checkout form
 
         cy.get(SauceLabsCheckoutInfo.h3ErrorMessage)
@@ -40,18 +34,7 @@ describe('Sauce Labs Demo - Unhappy paths - Check out info', () => {
     });
 
     it(`should require a zip code to be entered when checking out an item`, () => {
-      
-        //add an item from product list to cart
-        SauceLabsProductPage.addToCart(productData.productList[0].name);
-        SauceLabsProductPage.navigateToCart();
-
-        cy.get(SauceLabsProductPage.spanShoppingCartBadge)
-        .should('exist');
-
-        cy.get(SauceLabsProductPage.spanShoppingCartBadge) // check that an item is in the cart
-        .should('have.text',1);
-
-        cy.get(SauceLabsCart.btnCheckout).click();
+    
         SauceLabsCheckoutInfo.completeCheckoutFormWithoutZipCode(faker.name.firstName(),faker.name.lastName()); //enter customer info in checkout form
 
         cy.get(SauceLabsCheckoutInfo.h3ErrorMessage)
